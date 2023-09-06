11a5a674bd5f054f9db23df98d3e94964ba801f0
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-05 19:54:55 -0600

    Terminando clase de concatenación

--- 3. Variables y tipo de datos/02-variables-y-tipos/variables.py
+++ 3. Variables y tipo de datos/02-variables-y-tipos/variables.py
@@ -2,15 +2,26 @@
 
 # Python es un lenguaje debilmente tipado
 
+# Crear variables y mostrar un valor
 texto = "Master en Python"
 texto2 = "Con Victor Robles"
 numero = 45
 decimal = 6.7
 #Constante
 PI = 3.1416 
-print(texto)
-print(texto2)
-print(numero)
-print(type(decimal))
+# print(texto)
+# print(texto2)
+# print(numero)
+# print(type(decimal))
 
+#Concatenación de cadenas de texto
 
+nombre = "Josué"
+apellidos = "Martínez Carrasco"
+web = "esaujosue.dev"
+
+print(nombre + " " + apellidos +" " + web)
+# Otro tipo de concatenación utilizando f (Recomendado)
+print(f"{nombre} {apellidos} - {web}")
+# Otro tipo de concatenación utilizando el método format()
+print("Hola me llamo {} {} y mi web es: {}".format(nombre,apellidos, web))
