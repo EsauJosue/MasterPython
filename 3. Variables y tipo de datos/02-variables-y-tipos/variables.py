9902722b1e15000fae01139a2fe63cf157f04bd5
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-05 18:24:42 -0600

    variables y tipos de datos

---
+++ 3. Variables y tipo de datos/02-variables-y-tipos/variables.py
@@ -0,0 +1,16 @@
+# Una variable es un contenedro de información que dentyro guardara un dato, se pueden crear muchas variables y que cada una tenga un dato distinto
+
+# Python es un lenguaje debilmente tipado
+
+texto = "Master en Python"
+texto2 = "Con Victor Robles"
+numero = 45
+decimal = 6.7
+#Constante
+PI = 3.1416 
+print(texto)
+print(texto2)
+print(numero)
+print(type(decimal))
+
+
