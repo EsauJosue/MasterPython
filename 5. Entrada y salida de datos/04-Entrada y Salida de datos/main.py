95d3d96320f590ca95c77abbe2f8d988aaf4c9e5
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-09 14:40:11 -0600

    Entrada y salida de datos

---
+++ 5. Entrada y salida de datos/04-Entrada y Salida de datos/main.py
@@ -0,0 +1,11 @@
+# Entrada y Salida de datos 
+
+#Entrada
+nombre = input("Cuál es su nombre?: ")
+edad = input("Cual es tu edad: ")
+
+
+#Salida 
+
+print("El nombre es: {}".format(nombre))
+print("La esdad es: {}".format(str(edad)))
