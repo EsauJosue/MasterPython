9f3efbc51f2e8681e4a3cf5de4e581512ad6c7fc
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-09 16:13:03 -0600

    Operadores de comparación

---
+++ 6. Estructuras de Control - Condicionales/05-Condicionales/main.py
@@ -0,0 +1,17 @@
+#Condicional
+
+print("Ejemplo 1")
+edad = input("Cuál es tu edad?")
+if int(edad) >= 18:
+    print("Eres mayor de edad")
+else: 
+    print("No eres mayor de edad. Niño")
+
+#Opradores de comparación 
+
+# == Igual 
+# != Diferente 
+# < menor que 
+# > mayor que 
+# <= menor o igual
+# >= mayor o igual 
