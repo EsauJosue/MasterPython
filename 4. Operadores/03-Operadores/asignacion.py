642fc8d581e39feb08ad3a6ef0f92c620d408cf3
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-09 14:24:08 -0600

    Operadores de asignación

---
+++ 4. Operadores/03-Operadores/asignacion.py
@@ -0,0 +1,15 @@
+#Operadores de asignacion 
+
+edad = 55
+
+print("Hola tengo {}".format(edad))
+
+#Con el operador += primero a la variable edad se le suna cinco y después se asigna el valor a la variable edad
+#Esto es equivalente a edad = edad + 5
+edad += 5
+print("Ahora tengo {}".format(edad))
+
+edad -=15
+print("Ahora tengo {}".format(edad))
+
+#Operadores de incremento y decremento 
