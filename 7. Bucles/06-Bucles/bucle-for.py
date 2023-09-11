54590c4a3aea6fc2e3f4376b6e6295a73a67d227
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-10 20:31:49 -0600

    Agregando un else al for

--- 7. Bucles/06-Bucles/bucle-for.py
+++ 7. Bucles/06-Bucles/bucle-for.py
@@ -7,5 +7,5 @@ numero_usuario = int(input("Que número quieres la tabla de multiplicar"))
 
 for numero_tabla in range(0,10):
     print("{} x {} = {}".format(numero_usuario,numero_tabla,numero_usuario*numero_tabla))
-
-     
+else: #En python también se pueden agregar else cuando se usa un for 
+    print("Tabla finalizada")
