11d1b6431c441767a5b3d595ace1f2f17ba61392
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-11 19:18:43 -0600

    Ejercicio 4: Calculadora con dos numeros

---
+++ 8. Ejercicios/4. ejercicio/ejercicio4.py
@@ -0,0 +1,13 @@
+# Calculadora con dos numeros ingresados por el usuario
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+print("Resultados de las operaciones")
+print("El resultado de {} + {} = {}".format(num1,num2,num1+num2))
+print("El resultado de {} - {} = {}".format(num1,num2,num1-num2))
+print("El resultado de {} * {} = {}".format(num1,num2,num1*num2))
+print("El resultado de {} / {} = {}".format(num1,num2,num1/num2))
+
+
+
