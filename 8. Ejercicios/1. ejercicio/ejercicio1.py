fb2b295173f7d626b57e0f1e66c38dfe044f564b
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-11 18:50:57 -0600

    Ejercicio 1. done

---
+++ 8. Ejercicios/1. ejercicio/ejercicio1.py
@@ -0,0 +1,6 @@
+# Ejercicio 1, crear dos variables, una pais y otra continente, mostrar sus variables y el tipo de dato
+
+pais = "México"
+continente = "América"
+
+print("El nombre del país es {} \n su Continente es {}, y los tipos de datos son los siguientes. \n Pais: {} \n Continente: {}".format(pais,continente,type(pais),type(continente)))
