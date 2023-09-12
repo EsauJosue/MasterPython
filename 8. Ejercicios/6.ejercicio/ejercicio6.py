6f6b95ea0d2368de262391eb8c1ff785abcc2024
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-11 19:41:32 -0600

    Agregandole cabecera a cada tabla del ejercicio 6

--- 8. Ejercicios/6.ejercicio/ejercicio6.py
+++ 8. Ejercicios/6.ejercicio/ejercicio6.py
@@ -3,5 +3,6 @@ tabla = 1
 
 for tabla in range(1,11):
     numero = 1
+    print(f"Tabla del {numero}")
     for numero in range(1,11):
         print("{} x {} = {}".format(tabla,numero,numero*tabla))
