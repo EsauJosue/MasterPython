d619ed3430bdfa9de26323e33825104b3efef810
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-12 19:37:53 -0600

    Ejercicio 9

---
+++ 8. Ejercicios/9. ejercicio/ejercicio9.py
@@ -0,0 +1,9 @@
+
+passw = 0
+
+while passw != 111:
+    numero = int(input("Ingrese un numero: "))
+    passw = numero
+    print("El número que elegiste es {} ".format(passw))
+else: 
+    print("Se eligió el numero para finalizar el programa")
