b6dec62ea1e14761aef1299cfbb1d15be548076a
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-12 19:25:05 -0600

    Ejercicio 8. Porcentaje de una cantidad

---
+++ 8. Ejercicios/8. ejercicio/ejercicio8.py
@@ -0,0 +1,3 @@
+cantidad = int(input("Ingrese la cantidad: "))
+porcentaje = int(input("Ingrese el porcentaje del 1 al 100%: "))
+print(f"El {porcentaje}% de {cantidad} = {(cantidad*porcentaje)/100}")
