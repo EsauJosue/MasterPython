8a43413cc4c28d8e708b5deb68fd8c4f10a877e6
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-14 19:59:40 -0600

    Funciones

---
+++ 9.Funciones/08-funciones/main.py
@@ -0,0 +1,17 @@
+# Funciones: 
+# Una función es un conjunto de instrucciones agrupadas bajo un nombre concreto que pueden reutilizarse invocando a la función tantas veces como sea necesario
+
+# def nombreDeMiFuncion(parametros):
+#     Bloque de instrucciones
+
+# Ejemplo 1
+
+def muestraNombres():
+    print("Josué")
+    print("Esaú")
+    print("Juan")
+    print("Francisco")
+    print("Nestor")
+    print("\n")
+
+muestraNombres()
