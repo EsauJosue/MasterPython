b963b98dce5c61dc0a957831425f19a892d15913
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-17 09:53:03 -0600

    Comprobar variables vacias con la función len y además contabilizar el número de carácteres

--- 9.Funciones/08-funciones/predefinidas.py
+++ 9.Funciones/08-funciones/predefinidas.py
@@ -9,3 +9,9 @@ print(frase.strip())
 year = 2023
 print(year)
 del year
+#Comprobar variables vacias
+texto = " ff "
+if len(texto) <= 0: #Con la función len se pueden contar el número de caracteres que tiene una variable.
+    print("La variable esta vacia")
+else: 
+    print("La variable tiene contenido: {} carácteres".format(len(texto)))
