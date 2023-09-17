bd3ecd2b52a4dc7bf39b8a474e043052ede27949
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-17 09:50:18 -0600

    Funciones: limpiar espacios con función strip()

--- 9.Funciones/08-funciones/predefinidas.py
+++ 9.Funciones/08-funciones/predefinidas.py
@@ -2,3 +2,6 @@ nombre = "Esau Josue"
 #Funciones generales
 print(nombre)
 print(f"El tipo de la variable es: {type(nombre)}")
+#Limpiar espacios
+frase = "     mi  contenido    "
+print(frase.strip())
