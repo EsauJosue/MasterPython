da7e42387974017185d85450496cbbd43bdcea6c
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-17 09:51:28 -0600

    Funciones: eliminar variables con la palabra reservada del

--- 9.Funciones/08-funciones/predefinidas.py
+++ 9.Funciones/08-funciones/predefinidas.py
@@ -5,3 +5,7 @@ print(f"El tipo de la variable es: {type(nombre)}")
 #Limpiar espacios
 frase = "     mi  contenido    "
 print(frase.strip())
+#Eliminar variables
+year = 2023
+print(year)
+del year
