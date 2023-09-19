5c47775509f3e0a87fd9153da490fa4171e90078
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-09-19 12:08:16 -0600

    Funciones para rabajar listas

---
+++ 10. Listas/predefinidas.py
@@ -0,0 +1,7 @@
+cantantes = ['2pac','Drake', 'Bad Bunny', 'Julio Iglesias']
+numeros = [1,2,5,8,3,4]
+
+#Ordenar
+print(numeros)
+numeros.sort() #Con el método sort podemos ordenar una lista
+print(numeros)
