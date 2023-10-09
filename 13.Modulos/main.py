e9b5d7dc563e1b2884e554200c910dbf78cea31c
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-10-09 12:26:22 -0600

    Importando módulos propios

--- 13.Modulos/main.py
+++ 13.Modulos/main.py
@@ -2,5 +2,12 @@ import mimodulo
  #Para importar un modulo es necesario utilizar la palabra import
 print(mimodulo.holaMundo("Josue Martinez"))
 
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+print(f"La suma de los númeroes {num1} + {num2} es: {mimodulo.suma(num1,num2) }")
+
+
+
 
 
--- 13.Modulos/mimodulo.py
+++ 13.Modulos/mimodulo.py
@@ -1,3 +1,6 @@
 
 def holaMundo(nombre):
     return f"Hola que tal estas, {nombre}"
+def suma(a,b):
+    suma = a + b
+    return suma
